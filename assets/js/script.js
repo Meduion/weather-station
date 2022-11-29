192cf0d8c34eaf54d1a75594a967f755395a462c
Brian Wyatt <brianlwyatt@gmail.com>
2022-11-29 16:40:35 -0400

    added bootstrap to support five column layout for five day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,14 @@ var cityNameEl = document.querySelector('#city-name')
 var tempEl = document.querySelector('#temp')
 var windEl = document.querySelector('#wind')
 var humidityEl = document.querySelector('#humidity')
+var fiveDayHeadingEl = document.querySelector("#five-day-heading")
+var dayOneEl = document.querySelector("#day-one")
+var dayTwoEl = document.querySelector("#day-two")
+var dayThreeEl = document.querySelector("#day-three")
+var dayFourEl = document.querySelector("#day-four")
+var dayFiveEl = document.querySelector("#day-five")
 
-function searchButton (event) {
+function searchButton(event) {
   event.preventDefault();
 
   var searchEntry = document.querySelector('#search-text').value;
@@ -36,27 +42,44 @@ function searchButton (event) {
         pastSearch.classList.add('btn', 'btn-primary', 'btn-block');
         pastSearch.textContent = citySearched;
         searchFieldEl.appendChild(pastSearch);
-     }
+      }
 
-  var currentWeather = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=' + apiKey + '&units=metric';
+      var currentWeather = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=' + apiKey + '&units=metric';
 
-  fetch(currentWeather)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-      cityNameEl.textContent = data.name + '   ';
-      var icon = data.weather[0].icon;
-      document.querySelector('#weather-icon').src = 'http://openweathermap.org/img/wn/' + icon + '@2x.png'
-      tempEl.textContent = 'Current Temperature: ' + data.main.temp + ' ° C';
-      windEl.textContent = 'Current Wind Speed: ' + data.wind.speed + ' kph';
-      humidityEl.textContent = 'Current Humidity: ' + data.main.humidity + ' %';
-            
-    })
+      fetch(currentWeather)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          // console.log(data);
+          cityNameEl.textContent = data.name + '   ';
+          var icon = data.weather[0].icon;
+          document.querySelector('#weather-icon').src = 'http://openweathermap.org/img/wn/' + icon + '@2x.png'
+          tempEl.textContent = 'Current Temperature: ' + data.main.temp + ' ° C';
+          windEl.textContent = 'Current Wind Speed: ' + data.wind.speed + ' kph';
+          humidityEl.textContent = 'Current Humidity: ' + data.main.humidity + ' %';
+          
+          var fiveDayForecast = 'https://api.openweathermap.org/data/2.5/forecast/?lat=' + lat + '&lon=' + lon + '&appid=' + apiKey + '&units=metric';
 
-  
-  
+          fetch(fiveDayForecast)
+            .then(function(response) {
+              return response.json();
+            })
+            .then(function (data) {
+              console.log(data);
+              fiveDayHeadingEl.textContent = 'Five Day Forecast:';
+              var dayOneDate = data.list[7].dt_txt;
+              dayOneEl.textContent = dayOneDate;
+              var dayTwoDate = data.list[15].dt_txt;
+              dayTwoEl.textContent = dayTwoDate;
+              var dayThreeDate = data.list[23].dt_txt;
+              dayThreeEl.textContent = dayThreeDate;
+              var dayFourDate = data.list[31].dt_txt;
+              dayFourEl.textContent = dayFourDate;
+              var dayFiveDate = data.list[39].dt_txt;
+              dayFiveEl.textContent = dayFiveDate;
+            })
+        })
     });
 }
 
